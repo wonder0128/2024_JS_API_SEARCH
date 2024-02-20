0099ef1ab2690e253af52890e7ca561f717201fe
roze <wonder_0128@naver.com>
2024-02-20 13:09:13 +0900

    20240220 -end

--- api/movie.js
+++ api/movie.js
@@ -1,10 +1,12 @@
 import fetch from 'node-fetch'
 
+const {APIKEY} = process.env;
+
 export default async function handler(request, response){
   const {title, page, id} = JSON.parse(request.body);
   const url = id 
-    ? `https://www.omdbapi.com/?apikey=159d0449&i=${id}&plot=full`
-    : `https://www.omdbapi.com/?apikey=159d0449&s=${title}&page=${page}`;
+    ? `https://www.omdbapi.com/?apikey=${APIKEY}&i=${id}&plot=full`
+    : `https://www.omdbapi.com/?apikey=${APIKEY}&s=${title}&page=${page}`;
     const res = await fetch(url);
     const json = await res.json();
 
